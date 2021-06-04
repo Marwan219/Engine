63e61a27014f7b0b22f608276fb85f84f7323a5b
marwan diaaeldeen <marwandeya219@gmail.com>
2021-06-04 21:53:18 +0200

    Textures try

--- Sandbox/src/SandoboxApp.cpp
+++ Sandbox/src/SandoboxApp.cpp
@@ -21,7 +21,7 @@ public:
 			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
-		std::shared_ptr<Engine::VertexBuffer> vertexBuffer;
+		Engine::Ref<Engine::VertexBuffer> vertexBuffer;
 		vertexBuffer.reset(Engine::VertexBuffer::Create(vertices, sizeof(vertices)));
 		Engine::BufferLayout layout = {
 			{ Engine::ShaderDataType::Float3, "a_Position" },
@@ -31,28 +31,31 @@ public:
 		m_VertexArray->AddVertexBuffer(vertexBuffer);
 
 		uint32_t indices[3] = { 0, 1, 2 };
-		std::shared_ptr<Engine::IndexBuffer> indexBuffer;
+		Engine::Ref<Engine::IndexBuffer> indexBuffer;
 		indexBuffer.reset(Engine::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 		m_VertexArray->SetIndexBuffer(indexBuffer);
 
 		m_SquareVA.reset(Engine::VertexArray::Create());
 
-		float squareVertices[3 * 4] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.5f,  0.5f, 0.0f,
-			-0.5f,  0.5f, 0.0f
+		float squareVertices[5 * 4] = {
+			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+			 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+			 0.5f,  0.5f, 0.0f, 1.0f, 1.0f,
+			-0.5f,  0.5f, 0.0f, 0.0f, 1.0f
 		};
 
-		std::shared_ptr<Engine::VertexBuffer> squareVB;
+		Engine::Ref<Engine::VertexBuffer> squareVB;
 		squareVB.reset(Engine::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
 		squareVB->SetLayout({
-			{ Engine::ShaderDataType::Float3, "a_Position" }
+
+			{ Engine::ShaderDataType::Float3, "a_Position" },
+			{ Engine::ShaderDataType::Float2, "a_TexCoord" }
+
 			});
 		m_SquareVA->AddVertexBuffer(squareVB);
 
 		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
-		std::shared_ptr<Engine::IndexBuffer> squareIB;
+		Engine::Ref<Engine::IndexBuffer> squareIB;
 		squareIB.reset(Engine::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
 		m_SquareVA->SetIndexBuffer(squareIB);
 
@@ -122,6 +125,41 @@ public:
 		)";
 
 		m_FlatColorShader.reset(Engine::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
+
+		std::string textureShaderVertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec2 a_TexCoord;
+			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+			out vec2 v_TexCoord;
+			void main()
+			{
+				v_TexCoord = a_TexCoord;
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string textureShaderFragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+			in vec2 v_TexCoord;
+			
+			uniform sampler2D u_Texture;
+			void main()
+			{
+				color = texture(u_Texture, v_TexCoord);
+			}
+		)";
+
+		m_TextureShader.reset(Engine::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+
+		m_Texture = Engine::Texture2D::Create("assets/textures/Checkerboard.png");
+
+		std::dynamic_pointer_cast<Engine::OpenGLShader>(m_TextureShader)->Bind();
+		std::dynamic_pointer_cast<Engine::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
 	}
 
 
@@ -167,7 +205,11 @@ public:
 			}
 		}
 
-		Engine::Renderer::Submit(m_Shader, m_VertexArray);
+		m_Texture->Bind();
+		Engine::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+
+		// Triangle
+		// Engine::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Engine::Renderer::EndScene();
 	}
@@ -186,11 +228,13 @@ public:
 	}
 
 	private:
-		std::shared_ptr<Engine::Shader> m_Shader;
-		std::shared_ptr<Engine::VertexArray> m_VertexArray;
+		Engine::Ref<Engine::Shader> m_Shader;
+		Engine::Ref<Engine::VertexArray> m_VertexArray;
 
-		std::shared_ptr<Engine::Shader> m_FlatColorShader;
-		std::shared_ptr<Engine::VertexArray> m_SquareVA;
+		Engine::Ref<Engine::Shader> m_FlatColorShader, m_TextureShader;
+		Engine::Ref<Engine::VertexArray> m_SquareVA;
+
+		Engine::Ref<Engine::Texture2D> m_Texture;
 
 		Engine::OrthographicCamera m_Camera;
 		glm::vec3 m_CameraPosition;
@@ -210,7 +254,7 @@ public:
 
 	Sandbox()
 	{
-		pus	Layer(new TestLayer());
+		pushLayer(new TestLayer());
 		//pushOverlay(new Engine::LayerImGui());
 	}
 
